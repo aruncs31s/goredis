82cfc0ff70718125b3c9cfd224f1b33d8c21b083
Arun CS <aruncs31ss@gmail.com>
2026-07-30 11:06:37 +0530

    added examples

---
+++ examples/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/aruncs31s/goredis"
+	"github.com/redis/go-redis/v9"
+)
+
+var redisClient *redis.Client
+
+func init() {
+	redisClient = redis.NewClient(
+		&redis.Options{
+			Addr:     "localhost:8998",
+			Password: "greenIsBest",
+		},
+	)
+}
+
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func (u User) Display() {
+	fmt.Printf("ID: %d , Name: %s \n", u.ID, u.Name)
+}
+
+func main() {
+
+	ctx := context.Background()
+	// Cache a user under the "users" tag
+	user, err := goredis.CacheGetOrFetch(ctx, redisClient, "user:42", 5*time.Minute,
+		func() (*User, error) {
+			return &User{ID: 42, Name: "Alice"}, nil
+		},
+		"users",
+	)
+	user, err = goredis.CacheGetOrFetch(ctx, redisClient, "user:43", 5*time.Minute,
+		func() (*User, error) {
+			return &User{ID: 43, Name: "Alice"}, nil
+		},
+		"users",
+	)
+	if err != nil {
+		panic(err)
+	}
+	if user == nil {
+		goredis.InvalidateByTags(ctx, redisClient, "users")
+		return
+	}
+	user.Display()
+
+	// After a user update, clear all cached data tagged "users"
+	goredis.InvalidateByTags(ctx, redisClient, "users")
+	user, err = goredis.CacheGetOrFetch(ctx, redisClient, "user:42", 5*time.Minute,
+		func() (*User, error) {
+			return nil, nil
+		},
+		"users",
+	)
+	if user != nil {
+		user.Display()
+		return
+	}
+	fmt.Println("User is nil")
+}
